3c5546a30c14a45e8edac400337ea3eeef7bc9f2
Max Larsson <maxslarsson@gmail.com>
2022-01-19 01:07:35 -0500

    Added new base path for website

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import preprocess from "svelte-preprocess";
 // import adapter from "@sveltejs/adapter-auto";
 import adapter from "@sveltejs/adapter-static";
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
@@ -9,6 +11,13 @@ const config = {
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
+
+    paths: {
+      base: dev ? '' : '/coding-puzzles',
+    },
+    // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+    // For example, instead of '_app', use 'app_', 'internal', etc.
+    // appDir: 'app',
 
     vite: {
       css: {
