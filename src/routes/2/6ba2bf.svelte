94fb2dccea66ca974795700f9f47e7597cdd9082
Max Larsson <maxslarsson@gmail.com>
2022-01-18 20:45:16 -0500

    Added puzzle 2

---
+++ src/routes/2/6ba2bf.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Puzzle from "$lib/Puzzle.svelte";
+</script>
+
+<Puzzle puzzleNumber="2">
+    <img src="/2/location.png" width="75%"/>
+    <p>To find the passcode, <bold>round</bold> the latitude and longitude of where the picture was taken to four decimal places each, add them together, and multiply them by 10,000 to get rid of the decimal point.</p>
+</Puzzle>
