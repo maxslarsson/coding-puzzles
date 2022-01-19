cfbaabb5609cc9a94a6c724e892a8273d50854c2
Max Larsson <maxslarsson@gmail.com>
2022-01-19 00:37:12 -0500

    Changed the TODOs in puzzle 4

--- src/routes/4/143b61.svelte
+++ src/routes/4/143b61.svelte
@@ -57,12 +57,9 @@
     let uid = 1;
 
     let todos = [
-        { id: uid++, done: false, description: 'write some docs' },
-        { id: uid++, done: false, description: 'start writing blog post' },
-        { id: uid++, done: true,  description: 'buy some milk' },
-        { id: uid++, done: false, description: 'mow the lawn' },
-        { id: uid++, done: false, description: 'feed the turtle' },
-        { id: uid++, done: false, description: 'fix some bugs' },
+        { id: uid++, done: true, description: 'write source code' },
+        { id: uid++, done: true, description: 'fix some bugs' },
+        { id: uid++, done: false, description: 'find the password' },
     ];
 
     async function add(input) {
