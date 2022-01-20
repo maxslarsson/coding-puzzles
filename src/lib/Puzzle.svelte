12798e2db3859da25114826ed8fa7d98cbf999d8
Max Larsson <maxslarsson@gmail.com>
2022-01-20 13:59:44 -0500

    Updated Puzzle such that the Navigator is optional

--- src/lib/Puzzle.svelte
+++ src/lib/Puzzle.svelte
@@ -3,12 +3,17 @@
     import Navigator from "$lib/Navigator.svelte";
 
     export let puzzleNumber;
+
+    // This is a string, because when it is passed as a prop, it is converted to a string. Thus, there is no type error when I compare it to a string.
+    export let showNavigator = "true";
 </script>
 
 <section>
     <Header puzzleNumber={puzzleNumber} />
     <slot />
-    <Navigator puzzleNumber={puzzleNumber} />
+    {#if (showNavigator === 'true')}
+        <Navigator puzzleNumber={puzzleNumber} />
+    {/if}
 </section>
 
 <style lang="scss">
--- src/lib/Transition.svelte
+++ src/lib/Transition.svelte
@@ -1,10 +1,10 @@
 <script>
     import { fly } from 'svelte/transition';
-    export let refresh = ``;
+    export let refresh;
 </script>
 
 {#key refresh}
-    <div in:fly={{ delay: 150, duration: 150, y: -20, }} out:fly={{ duration: 150,  y: 20, }}>
+    <div in:fly={{ delay: 150, duration: 150, y: -20 }} out:fly={{ duration: 150,  y: 20 }}>
         <slot />
     </div>
 {/key}
