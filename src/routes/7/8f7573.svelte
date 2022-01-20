b92aa411a7a70aed03d1d11c0ef7e0105a32f740
Max Larsson <maxslarsson@gmail.com>
2022-01-20 14:25:38 -0500

    Added puzzle 7

---
+++ src/routes/7/8f7573.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Puzzle from "$lib/Puzzle.svelte";
+</script>
+
+<Puzzle puzzleNumber="7">
+    <img src="/7/spongebob.png" />
+    <p class="hidden">The password starts with the number 9</p>
+</Puzzle>
---
+++ src/routes/7/_data/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+stegano-lsb hide -i ./spongebob.png -m "99d116" -o ../../../../static/7/spongebob.png
