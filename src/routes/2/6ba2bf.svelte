1783802cc2cf5545876e0d30ab02a312df15a745
Max Larsson <maxslarsson@gmail.com>
2022-01-19 19:12:11 -0500

    Updated width of image on puzzle 2

--- src/routes/2/6ba2bf.svelte
+++ src/routes/2/6ba2bf.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <Puzzle puzzleNumber="2">
-    <img src="/2/location.png" width="75%"/>
+    <img src="/2/location.png" width="65%"/>
     <p>To find the passcode, <bold>round</bold> the latitude and longitude of where the picture was taken to four decimal places each, add them together, and multiply them by 10,000 to get rid of the decimal point.</p>
 </Puzzle>
