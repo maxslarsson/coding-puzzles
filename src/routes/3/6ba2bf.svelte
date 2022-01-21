57c5a271a67e73e9c57133881ccc0a1356ebde68
Max Larsson <maxslarsson@gmail.com>
2022-01-21 15:18:08 -0500

    Made puzzle 3 more doable

--- src/routes/3/6ba2bf.svelte
+++ src/routes/3/6ba2bf.svelte
@@ -4,5 +4,5 @@
 
 <Puzzle puzzleNumber="3">
     <img src="/3/location.png" width="65%"/>
-    <p>To find the passcode, <bold>round</bold> the latitude and longitude of where the picture was taken to four decimal places each, add them together, and multiply them by 10,000 to get rid of the decimal point.</p>
+    <p>To find the passcode, <bold>cut</bold> both the latitude and longitude of where the picture was taken to three decimal places each, add them together, and multiply them by 1,000 to get rid of the decimal point.</p>
 </Puzzle>
